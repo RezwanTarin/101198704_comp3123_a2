32110cd2ccfc6b1d6dbfcbd4246a96eede0710ae
https://github.com/RezwanTarin/101198704_comp3123_a2.git <rezwantarin@Rezwans-MacBook-Air.local>
2020-12-06 22:49:34 -0500

    final CSS

--- src/App.js
+++ src/App.js
@@ -8,13 +8,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      weather  : null,
-      main  : null,
-      wind  : null,
-      sys  : null,
-      name  : null,
-      iconhref : null,
-      isMounted : false
+      name  : null, weather  : null, main  : null,
+      wind  : null, isMounted : false, system  : null,iconhref : null,
      };
   };
     
@@ -22,7 +17,7 @@ class App extends Component {
   componentDidMount() {
     axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=ffbf99f93a2893f01da7d1d6a1b6f78d`)
     .then(res => {
-        const { weather , main , wind , sys , name } = res.data;
+        const { name,weather , main , wind , sys } = res.data;
         let ref =  `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
         this.setState({ weather : weather[0] , main , wind , sys , name , iconhref : ref, isMounted:true });
     })
@@ -32,25 +27,34 @@ class App extends Component {
     const {  main , wind ,  name , iconhref, isMounted  } = this.state;
     if(isMounted){
       return (
-        <div >
+        <div className='App' >
+          <div className='container'>
           <table>
+            <header>
             <h3>Weather App</h3>
             <tr> {name}</tr>
-            <tr><Image src={iconhref}/></tr>
+            <tr ><Image src={iconhref} className='weather-icon'/></tr>
+            </header>
+            <main>
             <tr>{main.weather}</tr>
-            <tr>Tempreture: {main.temp}</tr>
+            <tr className='temperature'>Tempreture: {main.temp}</tr>
             <tr>Feeling : {main.feels_like}</tr>
-            <tr>Minimum Tempreture: {main.temp_min}</tr>
-            <tr>Maximum Tempreture: {main.temp_max}</tr>
-            <tr>Humidity: {main.humidity}</tr>
+            <tr className='hi-lo'>Minimum: {main.temp_min}</tr>
+            <tr className='hi-lo'>Maximum: {main.temp_max}</tr>
+            </main>
+            <footer>
+            <tr lassName='weather-prop'>Humidity: {main.humidity}</tr>
             <tr>Wind: {wind.speed }</tr>
+            </footer>
           </table>
+          </div>
         </div>
       );
     }else{
       return(
-        <text>Try again</text>
+        <tex> Try Again </tex>
       )
+      
     }
   }
   
